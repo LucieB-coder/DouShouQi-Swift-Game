3b7eae7cddab7f20c5724024fd2dade4d8831086
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-12 11:59:10 +0100

    :sparkles: Enum classes in model

---
+++ source/Model/Sources/Model/enums/Owner.swift
@@ -0,0 +1,14 @@
+//
+//  Owner.swift
+//  
+//
+//  Created by etudiant on 11/01/2024.
+//
+
+import Foundation
+
+enum Owner : String {
+    case player1 = "1"
+    case player2 = "2"
+    case noOne = "x"
+}
