5f800dd7ea278078bb90350dcbfda977f4c0a673
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-13 21:05:22 +0100

    :sparkles: Piece structure

---
+++ source/Model/Sources/Model/structures/Piece.swift
@@ -0,0 +1,20 @@
+//
+//  Piece.swift
+//
+//
+//  Created by etudiant on 12/01/2024.
+//
+
+import Foundation
+
+struct Piece {
+    let owner : Owner
+    let animal : Animal
+    
+    var descripion : String { "[\(self.owner.rawValue):\(self.animal)]" }
+    
+    init(owner: Owner, animal: Animal) {
+        self.owner = owner
+        self.animal = animal
+    }
+}
