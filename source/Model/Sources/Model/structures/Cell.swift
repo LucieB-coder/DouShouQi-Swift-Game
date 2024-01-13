6f565e966db222403929f88b711adb43d38f9fef
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-13 21:10:14 +0100

    :sparkles: Cell structure

---
+++ source/Model/Sources/Model/structures/Cell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell.swift
+//
+//
+//  Created by etudiant on 12/01/2024.
+//
+
+import Foundation
+
+struct Cell {
+    let cellType: CellType
+    let initialOwner : Owner
+    let piece : Piece?
+    
+    var description : String {
+        "\(self.piece == nil ? self.piece?.descripion : "ø") on \(self.cellType), \(self.initialOwner.rawValue)"
+    }
+    
+    init(cellType: CellType, initialOwner: Owner, piece: Piece?) {
+        self.cellType = cellType
+        self.initialOwner = initialOwner
+        self.piece = piece
+    }
+}
