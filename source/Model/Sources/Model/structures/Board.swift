a632f7fa24f0f69946f1e3befe24bfa391361d2c
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-14 14:03:31 +0100

    :sparkles: Board structure

---
+++ source/Model/Sources/Model/structures/Board.swift
@@ -0,0 +1,26 @@
+//
+//  Board.swift
+//
+//
+//  Created by etudiant on 12/01/2024.
+//
+
+import Foundation
+
+struct Board {
+    let nbRows : Int
+    let nbColumns : Int
+    private(set) var grid : [[Cell]]
+    
+    init?(grid: [[Cell]]) {
+        let rowSizes = Set(grid.map { $0.count })
+        guard rowSizes.count == 1 else {
+            // Rows have different sizes
+            return nil
+        }
+        self.nbRows = grid.count
+        self.nbColumns = rowSizes.first!
+        self.grid = grid
+    }
+}
+
