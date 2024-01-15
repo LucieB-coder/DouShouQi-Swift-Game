f787953ba7c2ea3549c643f1057dd64840a79f0a
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-15 14:25:55 +0100

    :sparkles: CountPieces method + some comments

--- source/Model/Sources/Model/structures/Board.swift
+++ source/Model/Sources/Model/structures/Board.swift
@@ -15,7 +15,11 @@ public struct Board {
     
     public init?(grid: [[Cell]]) {
         let firstRowSize : Int = grid.first?.count ?? 0
+        // allSatisfy enables to apply a condition to each row of the array
+        // returns true if all rows fills the condition
+        // returns false otherwise
         let areRowsEqualSize = grid.allSatisfy {$0.count == firstRowSize }
+        // guard is a control statement to execute some code if the condition is not met
         guard areRowsEqualSize else {
             // Rows have different sizes
             return nil
@@ -23,6 +27,14 @@ public struct Board {
         self.nbRows = grid.count
         self.nbColumns = grid.first?.count ?? 0
         self.grid = grid
+    }
+    
+    public func countPieces(of owner: Owner) -> Int {
+        // At the origin, the value is zero
+        // For each row of the board, we add the number of pieces belonging to the owner who we want to count his pieces
+        // $0 is the previous value
+        // $1 is the current row, which we filter to find the pieces of the owner and on which we execute a count action
+        return grid.reduce(0) { $0 + $1.filter { $0.piece?.owner == owner }.count }
     }
 }
 
