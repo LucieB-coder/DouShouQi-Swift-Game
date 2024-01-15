58caa3862679ae8594078b8746aac08286153dac
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-15 11:27:12 +0100

    :refactor: remove rawValue

--- source/Model/Sources/Model/enums/Owner.swift
+++ source/Model/Sources/Model/enums/Owner.swift
@@ -9,9 +9,15 @@ import Foundation
 
 public enum Owner : String, CustomStringConvertible {
 
-    case player1 = "1"
-    case player2 = "2"
-    case noOne = "x"
+    case player1
+    case player2
+    case noOne
     
-    public var description: String { self.rawValue }
+    public var description: String {
+        switch self {
+        case .player1: return "1"
+        case .player2: return "2"
+        case .noOne: return "x"
+        }
+    }
 }
