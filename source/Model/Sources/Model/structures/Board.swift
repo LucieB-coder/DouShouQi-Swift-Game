37093581fb7a2a84287ca40cd0ede2360b033a2b
etudiant <etudiant@MB-R-110-000-994.local>
2024-01-17 15:29:25 +0100

    :recycle: extract array extension

--- source/Model/Sources/Model/structures/Board.swift
+++ source/Model/Sources/Model/structures/Board.swift
@@ -7,17 +7,6 @@
 
 import Foundation
 
-public extension Array where Element : Collection {
-    // This function is mapping on all the elements in the array of arrays and apply the given function to it
-    func map(andApplyFunction function: (Element.Element)->Void){
-        for row in self {
-            for column in row {
-                function(column)
-            }
-        }
-    }
-}
-
 public struct Board {
     public let nbRows : Int
     public let nbColumns : Int
